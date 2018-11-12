fb00ce12b5872c7e0b85d5887f081fc903ebb188
fotol1 <fotol@bk.ru>
2018-11-13 00:21:46 +0300

    some changes

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ data = pd.read_csv('new_data.csv')
 from mayavi import mlab
 import numpy as  np
 
-
-
-
 temp_list = list(data['Theta [deg]'])
 
 x = []
 z = []
+y = []
+
 for elem in data.keys():
     if elem != 'Theta [deg]':
         entry = elem.find("Phi")+5
         phi = int(elem[entry:elem.find('deg')])
         x.extend([phi for i in range(len(temp_list))])
         z.extend(data[elem])
-y = []
+
 for _ in range(len(data.keys())-1):
     y.extend([i for i in range(len(temp_list))])
 
@@ -30,6 +29,4 @@ for i in range(len(x)):
 	Z.append(z[i]*np.sin(((y[i]-90)/57)))
 	
 mlab.points3d(X,Y,Z,z)
-#zin(mu), tube_radius=0.025, colormap='Spectral'mlab.mesh(x,y,z)
-#mlab.mesh(x,y,z,representation='wireframe',color=(0,0,0))
 a = int(input())
